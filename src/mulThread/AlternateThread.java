c2dc7d496116ab3fd560e65c10ee549fda5bc0b7
shsun <shsun@RUN-20170817VWQ>
2018-06-29 09:34:29 +0800

    代码格式化………………………………

--- src/mulThread/AlternateThread.java
+++ src/mulThread/AlternateThread.java
@@ -1,16 +1,10 @@
 package mulThread;
-
 /**
-
  * 子线程循环10次，主线程接着循环100次，如此循环50次的问题
-
  * @author zhaojw_420
-
  *
-
  */
 public class AlternateThread {
-
     public static void main(String[] args) {
         final Bussies bussies=new Bussies();
         MainThread mainThread=new MainThread(bussies);
@@ -21,21 +15,14 @@ public class AlternateThread {
         sub.start();
     }
 }
-
 class Bussies{
     private boolean flage=true; //flage为true时打印主线程，为false时打印子线程
-
-
     public void main(){
-
         synchronized(this){
         while (!flage) {
             try {
                 this.wait(); //为false时等待
-
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-
                 e.printStackTrace();
             }
          }
@@ -45,16 +32,12 @@ class Bussies{
         this.notifyAll();
         }
     }
-
     public void sub(){
         synchronized(this){
             while (flage) {
             try {
                 this.wait();//如果为true则等待
-
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-
                 e.printStackTrace();
             }
          }
@@ -66,11 +49,8 @@ class Bussies{
         }
     }
 }
-
 //主线程
-
 class MainThread implements Runnable{
-
     private Bussies bussies;
     public MainThread(Bussies bussies) {
         this.bussies = bussies;
@@ -87,14 +67,10 @@ class MainThread implements Runnable{
             bussies.main();
         }
     }
-
 }
 //子线程
-
 class SubThread implements Runnable{
-
     private Bussies bussies;
-
     public SubThread(Bussies bussies) {
         this.bussies = bussies;
     }
@@ -104,13 +80,10 @@ class SubThread implements Runnable{
     public void setBussies(Bussies bussies) {
         this.bussies = bussies;
     }
-
     @Override
     public void run() {
         for(int i=0;i<50;i++){
             bussies.sub();
         }
-
     }
-
 }
